8c16a8a8ffe53d5120721a3c7cf82064aaa61646
Leon Bogaert <leon@tim-online.nl>
2013-05-21 19:51:30 +0200

    Initial commit

---
+++ manifests/config.pp
@@ -0,0 +1,3 @@
+class hexchat::config {
+
+}
---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+class hexchat(
+	$package_name = params_lookup( 'package_name' ),
+	$enabled       = params_lookup( 'enabled' )
+  ) inherits hexchat::params {
+
+  	$ensure = $enabled ? {
+  		true => present,
+  		false => absent
+  	}
+
+	include hexchat::package, hexchat::config
+}
